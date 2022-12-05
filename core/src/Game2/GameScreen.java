4df14a95e8de37d90d1fa52b426c968096f01c91
KareemSinbel <kareem.sinbel@gmail.com>
2022-12-05 22:10:24 +0200

    -edited on temp.txt

--- core/src/Game2/GameScreen.java
+++ core/src/Game2/GameScreen.java
@@ -1,16 +1,26 @@
 package Game2;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.utils.viewport.*;
 import com.phoenix.MultipleScreen;
 
+
 public class GameScreen  implements Screen {
-    MultipleScreen hsson;
+    MultipleScreen game;
+    public enum State
+    {
+        PAUSE,
+        RUN,
+        //RESUME,
+        //STOPPED
+    }
+
+    private State state = State.RUN;
 
     //graphics
     private Texture background;
@@ -20,44 +30,76 @@ public class GameScreen  implements Screen {
     private float BackgroundMove;
 
     //World parameters
-    private float WorldWidth= Gdx.graphics.getWidth();
-    private float WorldHeight=Gdx.graphics.getHeight();
+    private final float WorldWidth= Gdx.graphics.getWidth();
+    private final float WorldHeight=Gdx.graphics.getHeight();
     public GameScreen(MultipleScreen screen){
-     hsson =screen;
+     game =screen;
     }
 
+    private Viewport viewport;
+    private Camera camera;
+
     @Override
-    public void show() {
+    public void show()
+    {
         batch=new SpriteBatch();
         //Screen
-        Camera camera1 = new OrthographicCamera();
-        background=new Texture("flappy-bird-assets-master/sprites/background-night.png");
-        Viewport viewport =new StretchViewport(WorldWidth,WorldHeight,camera1);
+        camera = new OrthographicCamera();
+        background = new Texture("flappy-bird-assets-master/sprites/background-night.png");
+        viewport = new FillViewport(WorldWidth,WorldHeight,camera);
     }
 
     @Override
-    public void render(float delta) {
-    BackgroundMove++;
-    BackgroundMove=BackgroundMove%WorldWidth;
-    batch.begin();
-    batch.draw(background,-BackgroundMove,0,WorldWidth,WorldHeight);
-    batch.draw(background,-BackgroundMove+WorldWidth,0,WorldWidth,WorldHeight);
-    batch.end();
+    public void render(float delta)
+    {
+        switch (state)
+        {
+            case RUN:
+                BackgroundMove++;
+                BackgroundMove=BackgroundMove%WorldWidth;
+                if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE))
+                {
+                    pause();
+                }
+                camera.update();
+                viewport.apply();
+                //batch.setProjectionMatrix(viewport.getCamera().combined);
+                batch.begin();
+                batch.draw(background,-BackgroundMove,0,WorldWidth,WorldHeight);
+                batch.draw(background,-BackgroundMove+WorldWidth,0,WorldWidth,WorldHeight);
+                batch.end();
+                break;
+
+            case PAUSE:
+                if(Gdx.input.isKeyPressed(Input.Keys.SPACE))
+                {
+                   resume();
+                }
+                camera.update();
+                batch.begin();
+                batch.draw(background,-BackgroundMove,0,WorldWidth,WorldHeight);
+                batch.draw(background,-BackgroundMove+WorldWidth,0,WorldWidth,WorldHeight);
+                batch.end();
+                break;
+        }
     }
+
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width,height);
     }
 
     @Override
-    public void pause() {
-
+    public void pause()
+    {
+        this.state = State.PAUSE;
     }
 
     @Override
-    public void resume() {
-
+    public void resume()
+    {
+        this.state = State.RUN;
     }
 
     @Override
--- desktop/src/com/phoenix/DesktopLauncher.java
+++ desktop/src/com/phoenix/DesktopLauncher.java
@@ -1,7 +1,5 @@
 package com.phoenix;
 
-import Game1.*;
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
@@ -11,7 +9,13 @@ public class DesktopLauncher
 	{
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(144);
+		config.useVsync(true);
+		config.setResizable(false);
+		//config.setFullscreenMode(Gdx.graphics.getDisplayMode());
+		int width = (int) ((Lwjgl3ApplicationConfiguration.getDisplayMode().width)*(55.0/100.0));  //Ratio of width @Kareem
+		int height = (int) ((Lwjgl3ApplicationConfiguration.getDisplayMode().height)*(75.0/100.0)); //Ratio of height @Kareem
+		config.setWindowedMode(width,height);
 		config.setTitle("Phoenix");
-		Lwjgl3Application app = new Lwjgl3Application(new MultipleScreen(), config);
+		new Lwjgl3Application(new MultipleScreen(), config);
 	}
 }
