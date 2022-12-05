fa0808917726ae53f4e001bb1f28ea0d9c2fd010
vodkagamed <mohamed2122418@gmail.com>
2022-12-05 22:18:13 +0200

    I added a photos of flap I made a GameScreen.java for flap

--- desktop/src/com/phoenix/DesktopLauncher.java
+++ desktop/src/com/phoenix/DesktopLauncher.java
@@ -1,7 +1,4 @@
 package com.phoenix;
-
-import Game1.*;
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
@@ -9,9 +6,14 @@ public class DesktopLauncher
 {
 	public static void main (String[] arg)
 	{
+		int Width=Lwjgl3ApplicationConfiguration.getDisplayMode().width;
+		int Height=Lwjgl3ApplicationConfiguration.getDisplayMode().height*90/100;
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(144);
+		config.setForegroundFPS(60);
 		config.setTitle("Phoenix");
+		config.setWindowedMode(Width,Height);
+		//config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
 		Lwjgl3Application app = new Lwjgl3Application(new MultipleScreen(), config);
+
 	}
 }
