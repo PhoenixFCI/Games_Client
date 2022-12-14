fa1764786020d17f0a8a9af23da1751696490d13
Malek <malekyasser10@gmail.com>
2022-12-14 11:31:22 +0200

    I made gravity for bird (Update function). Also made jump function for bird.

--- core/src/FlappyBird/GameScreen.java
+++ core/src/FlappyBird/GameScreen.java
@@ -2,12 +2,12 @@ package FlappyBird;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.*;
 import com.phoenix.MultipleScreen;
-
-import java.util.Vector;
 
 
 public class GameScreen  implements Screen {
@@ -27,14 +27,12 @@ public class GameScreen  implements Screen {
     //graphics
     private Texture background;
     private SpriteBatch batch;
-    private Objects player;
-
-    private final Vector<Objects>enemies= new Vector<>();
+    private Objects player,enemy;
     private Texture playerTexture,enemyTexture;
     //private Motions animation;
 
     //timing
-    private float BackgroundMove=0;
+    private float BackgroundMove;
 
     //World parameters
     private final float WorldWidth= Gdx.graphics.getWidth();
@@ -46,39 +44,24 @@ public class GameScreen  implements Screen {
     private Viewport viewport;
     private Camera camera;
 
-    private GameFont gameFont;
-    private GameFont scoreFont;
-    private int score=0;
     @Override
     public void show()
     {
         //camera and rendering things:
         batch=new SpriteBatch();
         camera = new OrthographicCamera();
-        viewport = new StretchViewport(WorldWidth,WorldHeight,camera);
-
-        //font
-        //fonts
-        String fontPath = "Flappy Bird Game/joystix.monospace-regular.ttf";
-        gameFont=new GameFont(fontPath,50,Color.BROWN,Color.BLACK,5);
-       scoreFont=new GameFont(fontPath,25,Color.WHITE,Color.BLACK,1);
+        viewport = new FillViewport(WorldWidth,WorldHeight,camera);
 
         //textures and objects in the game:
 
         background = new Texture("Flappy Bird Game/sprites/background-night.png");
         playerTexture =new Texture("Flappy Bird Game/sprites/bluebird-upflap.png");
-        player=new Objects(playerTexture, playerTexture.getWidth()*2, playerTexture.getHeight()*2,6,0);
+        player=new Objects(playerTexture, playerTexture.getWidth()*2, playerTexture.getHeight()*4,20,0);
         enemyTexture=new Texture("Flappy Bird Game/sprites/pipe-red.png");
-        int des=0;
-        for (int i = 0; i < 5; i++) {
-
-            Objects enemy = new Objects(enemyTexture, enemyTexture.getWidth(), enemyTexture.getHeight(), WorldWidth - 80 - (des), 0);
-            des+=200;
-            enemies.add(enemy);
-        }
-
+        enemy=new Objects(enemyTexture,enemyTexture.getWidth(),enemyTexture.getHeight(),WorldWidth-80,0);
 
     }
+
 
     @Override
     public void render(float delta)
@@ -91,8 +74,8 @@ public class GameScreen  implements Screen {
                 if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE))
                     pause();
 
-                if(Gdx.input.isKeyPressed(Input.Keys.D)){
-                    player.move(2,0);
+                if(Gdx.input.justTouched()){
+                    player.jump();
                     //I was testing how can I make Animations: by changing the photo everytime the user press button
                     player.setTexture(new Texture("Flappy Bird Game/sprites/redbird-downflap.png"));
                 }
@@ -100,36 +83,20 @@ public class GameScreen  implements Screen {
                 if(Gdx.input.isKeyPressed(Input.Keys.A))
                     player.move(-2,0);
 
-
-                if (!Gdx.input.isKeyPressed(Input.Keys.ANY_KEY))
-                   player.setTexture(new Texture("Flappy Bird Game/sprites/bluebird-upflap.png"));
-
-                //Testing Score function
-                for (int i = 0; i < enemies.size(); i++) {
-                    if (player.intersects(enemies.get(i).getCoordinates()))
-                        System.out.println("true");
+                if (!Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)) {
+                    player.update(delta);
+                    player.setTexture(new Texture("Flappy Bird Game/sprites/bluebird-upflap.png"));
                 }
-                for (int i = 0; i < enemies.size(); i++) {
-                    int temp =(int)enemies.get(i).getCoordinates().x;
-                    if (temp%2!=0)
-                        temp++;
-                    if (player.getCoordinates().x==temp)
-                        score++;
-
-                }
+                if (player.intersects(enemy.getCoordinates()))
+                    System.out.println("true");
 
                 camera.update();
                 viewport.apply();
                 batch.begin();
-                //moving background
                 batch.draw(background,-BackgroundMove,0,WorldWidth,WorldHeight);
                 batch.draw(background,-BackgroundMove+WorldWidth,0,WorldWidth,WorldHeight);
-                player.draw(batch);
-
-                for (int i = 0; i < enemies.size(); i++) {
-                    enemies.get(i).draw(batch);
-                }
-                scoreFont.draw(batch,"Score: "+score,5,WorldHeight-scoreFont.textHeight());
+                batch.draw(player.getTexture(),player.getPosition().x,player.getPosition().y);
+                enemy.draw(batch);
                 batch.end();
                 break;
 
@@ -142,7 +109,6 @@ public class GameScreen  implements Screen {
                 batch.begin();
                 batch.draw(background,0,0,WorldWidth,WorldHeight);
                 batch.draw(background,-BackgroundMove+WorldWidth,0,WorldWidth,WorldHeight);
-                gameFont.draw(batch,"Welcome\n"+ "nroibnto\n"+"n",Gdx.graphics.getWidth()/2-gameFont.textWidth()/2,Gdx.graphics.getHeight()/2- gameFont.textHeight()/2);
                 batch.end();
                 break;
         }
@@ -151,8 +117,7 @@ public class GameScreen  implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width,height,true);
-        batch.setProjectionMatrix(camera.combined);
+        viewport.update(width,height);
     }
 
     @Override
--- core/src/FlappyBird/Objects.java
+++ core/src/FlappyBird/Objects.java
@@ -1,16 +1,19 @@
 package FlappyBird;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
 public class Objects {
     //graphics
     private Texture texture;
 
     //dimensions&positions
-    private float width,height,xPos,yPos;
-
+    private float width,height,xPos,yPos ;
+    private Vector3 position= new Vector3(20,300,0) , velocity= new Vector3(0,0,0);
+    private static final int Gravity = -15;
     public Objects(Texture texture, float width, float height, float xPos, float yPos) {
         this.texture = texture;
         this.width = width;
@@ -18,6 +21,7 @@ public class Objects {
         this.xPos = xPos;
         this.yPos = yPos;
     }
+    public  Objects(){}
     //collision Detection
     public boolean intersects(Rectangle otherObject){
         Rectangle thisObject=new Rectangle(xPos,yPos,width,height);
@@ -30,11 +34,35 @@ public class Objects {
         batch.draw(texture,xPos,yPos,width,height);
     }
 
+    public Vector3 getPosition() {
+        return position;
+    }
 
-    public void move(int x,int y){
+    public void move(int x, float y){
         xPos+=x;
         yPos+=y;
     }
+    public void update(float dt){
+
+    velocity.add(0,Gravity,0);
+    velocity.scl(dt);
+    position.add(0,velocity.y,0);
+    velocity.scl(1/dt);
+        System.out.println("Update is working");
+    }
+    public  void jump(){
+        velocity.y = 300;
+
+    }
+
+
+
+    public void move(){}
+
+    public Texture getTexture() {
+        return texture;
+    }
+
     public void setTexture(Texture texture){
         this.texture=texture;
     }
